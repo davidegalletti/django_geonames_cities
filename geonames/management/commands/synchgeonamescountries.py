9a1f343f223b36fe8c8fa2754229649f0121b256
Davide Galletti <davide,galletti@gmail,com>
2024-02-08 11:33:25 +0100

    refs #1091 aggiunta e corretta con mappa codici importazione codice istat dei paesi

--- geonames/management/commands/synchgeonamescountries.py
+++ geonames/management/commands/synchgeonamescountries.py
@@ -24,7 +24,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         base_url = 'https://download.geonames.org/export/dump/'
-        istat_ute_url = 'https://www.istat.it/it/files//2011/01/Elenco-codici-e-denominazioni-unita-territoriali-estere.zip'
         # Let's import countries:
         country_dict = {}
         path_countries = settings.GEONAMES_DEST_PATH + "countryInfo.txt"
@@ -49,20 +48,5 @@ class Command(BaseCommand):
                             c.save()
                             country_dict[row[0]] = c
                 Country.objects.filter(code__in=country_2b_deleted).delete()
-                # l'aggiornamento del codice istat da istat_ute_url non funziona perché l'elenco dei paesi non è
-                # completo e alcuni codici degli stati non coincidono. Es: Codice ISO 3166 alpha2 UK su istat e GB su
-                # geonames
-                # path_istat_countries = settings.GEONAMES_DEST_PATH + "istat_ute.zip"
-                # istat_downloader = Downloader(istat_ute_url, path_istat_countries)
-                # istat_downloader.download()
-                # csv_file = open(istat_downloader.single_file_if_zip(['csv', 'Elenco-codici-e-denominazioni']),
-                #                 'rt', encoding='latin')
-                # csv_reader = csv.reader(csv_file, delimiter=';', quotechar="\\")
-                # for row in csv_reader:
-                #     if row[0] == "S":
-                #         if Country.objects.filter(code=row[11]).exists():
-                #             c = Country.objects.get(code=row[11])
-                #             c.it_codice_catastale = row[5]
-                #             c.save()
             except Exception as ex:
                 logger.error("Error %s" % str(ex))
