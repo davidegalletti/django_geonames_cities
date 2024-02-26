398105ef53f1d25d776e97978d0974b235b94421
Davide Galletti <davide,galletti@gmail,com>
2024-02-26 18:29:40 +0100

    fix migrations per modifiche modelli

--- geonames/migrations/0006_fixtures.py
+++ geonames/migrations/0006_fixtures.py
@@ -9,9 +9,8 @@ logger = logging.getLogger(__name__)
 
 def fixture_geonames(apps, schema_editor):
     try:
-        pass
-        # do nothing; it used to run commands that wouldn't work due to change in models that are
-        # managed in following migrations
+        management.call_command('synchgeonamescountries')
+        management.call_command('synchgeonames')
     except Exception as ex:
         logger.error("fixture_geonames: %s" % str(ex))
 
@@ -19,7 +18,7 @@ def fixture_geonames(apps, schema_editor):
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('geonames', '0001_initial'),
+        ('geonames', '0005_alter_country_it_codice_istat'),
     ]
 
     operations = [
