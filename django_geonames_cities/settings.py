d93502575567a71f15a727215513c38d0a65388c
Davide Galletti <davide,galletti@gmail,com>
2024-11-21 12:22:38 +0100

    0.14

--- django_geonames_cities/settings.py
+++ django_geonames_cities/settings.py
@@ -128,5 +128,5 @@ GEONAMES_DEST_PATH = '/tmp/'
 GEONAMES_INCLUDE_CITY_TYPES = ['PPL', 'PPLA', 'PPLA2', 'PPLA3', 'PPLA4', 'PPLC', 'PPLF', 'PPLG', 'PPLL', 'PPLS']
 
 # La versione va scritta qui, su setup.cfg e su geonames/__init__.py
-VERSION = '0.13'
-VERSION_COMMENT = 'autenticazione api'
+VERSION = '0.14'
+VERSION_COMMENT = 'fix url stati esteri istat'
