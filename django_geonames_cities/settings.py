77563c03716d956b30f86c20b4836bc7db9e3c77
Davide Galletti <davide,galletti@gmail,com>
2024-12-05 09:32:37 +0100

    0.15.1

--- django_geonames_cities/settings.py
+++ django_geonames_cities/settings.py
@@ -128,5 +128,5 @@ GEONAMES_DEST_PATH = '/tmp/'
 GEONAMES_INCLUDE_CITY_TYPES = ['PPL', 'PPLA', 'PPLA2', 'PPLA3', 'PPLA4', 'PPLC', 'PPLF', 'PPLG', 'PPLL', 'PPLS']
 
 # La versione va scritta qui, su setup.cfg e su geonames/__init__.py
-VERSION = '0.15'
-VERSION_COMMENT = 'aggiunta importazione codici istat e catastali per Country'
+VERSION = '0.15.1'
+VERSION_COMMENT = '#1479 Z999 apolide country_it_codice_catastale.py'
